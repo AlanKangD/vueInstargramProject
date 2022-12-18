3a03da5290410e7d4c9d6ae4049d493dbb2112de
alan <kno12350@gmail.com>
2022-12-18 23:08:41 +0900

    instargram project create

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="(a, i) in data" :key="i">
+    <POST :a="a"/>
+  </div>
+</template>
+
+<script>
+import POST from './Post.vue';
+
+export default {
+    name : 'Container',
+    components: {
+      POST : POST,
+  },
+  props : {
+    data : Array,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
